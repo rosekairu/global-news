0080c8cc2b8c2868abe221f24d7d9294290168c9
Rose Kairu <rosekairu@Roses-MacBook-Pro.local>
2025-02-19 19:19:12 +0300

    edited manage

--- manage.py
+++ manage.py
@@ -1,8 +1,9 @@
 import os
-
 from app import create_app
-app = create_app('development')
+
+config_name = os.getenv('FLASK_CONFIG', 'development')
+app = create_app(config_name)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
